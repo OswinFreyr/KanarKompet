9f141c73816e9ef0d3eafd14cdb1d5430b399fdd
OnyxBoi <152607901+OnyxBoi@users.noreply.github.com>
2024-10-22 10:56:31 +0200

    Installed Tailwind Css

--- app.vue
+++ app.vue
@@ -3,5 +3,9 @@ const { data } = await useFetch('/api/hello')
 </script>
 
 <template>
-  <pre>{{ data }}</pre>
+  <div>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
+  </div>
 </template>
