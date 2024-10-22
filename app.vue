d81ace85b39e4b57d488d77079f849138a14702b
OnyxBoi <152607901+OnyxBoi@users.noreply.github.com>
2024-10-22 10:58:35 +0200

    Removed test button

--- app.vue
+++ app.vue
@@ -3,9 +3,5 @@ const { data } = await useFetch('/api/hello')
 </script>
 
 <template>
-  <div>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Button
-    </button>
-  </div>
+  <pre>{{ data }}</pre>
 </template>
