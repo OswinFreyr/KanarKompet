f43adcc4e0c8f7b146d4bf22bf2494f39f889de5
inesbdl <bouadilin@gmail.com>
2024-10-23 13:40:08 +0200

    Debug route commentaireCompetition

--- KanarKompet_Back/routes/competitionRoute.js
+++ KanarKompet_Back/routes/competitionRoute.js
@@ -11,7 +11,7 @@ router.post("/", competitionController.createCompetition);
 router.post("/competitionCanard/:idCompetition/:idCanard", competitionController.addCanardToCompetition);
 router.post("/competitionAdmin/:idCompetition/:idAdmin", competitionController.addAdminToCompetition);
 router.post("/competitionLocalisation/:idCompetition/:idLocalisation", competitionController.addLocalisationToCompetition);
-router.post("/competitionComentaireCompetition/:idCompetition/:idCommentaireCompetition", competitionController.addCommentaireCompetitionToCompetition);
+router.post("/competitionCommentaireCompetition/:idCompetition/:idCommentaireCompetition", competitionController.addCommentaireCompetitionToCompetition);
 
 router.patch("/update/:id", competitionController.updateCompetition);
 
